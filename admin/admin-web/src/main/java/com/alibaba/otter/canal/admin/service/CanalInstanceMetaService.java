c6db84c1eaea2f6e2f24e567566dad2635909ec8
仙_梦如尘 <zhimin711@sina.com>
2020-06-03 08:51:31 +0800

    Meta集群配置调整

--- admin/admin-web/src/main/java/com/alibaba/otter/canal/admin/service/CanalInstanceMetaService.java
+++ admin/admin-web/src/main/java/com/alibaba/otter/canal/admin/service/CanalInstanceMetaService.java
@@ -3,7 +3,7 @@ package com.alibaba.otter.canal.admin.service;
 import com.alibaba.otter.canal.protocol.position.LogPosition;
 
 /**
- * Canal实例配置信息业务层接口
+ * Canal实例Meta信息业务层接口
  *
  * @author rewerma 2019-07-13 下午05:12:16
  * @version 1.0.0
