96e06f12690da711c861274efe0247088301ad03
仙_梦如尘 <zhimin711@sina.com>
2020-08-22 13:00:09 +0800

    合并Master

--- server/src/main/java/com/alibaba/otter/canal/server/CanalMQStarter.java
+++ server/src/main/java/com/alibaba/otter/canal/server/CanalMQStarter.java
@@ -23,7 +23,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-import com.alibaba.otter.canal.connector.core.util.Callback;
 
 public class CanalMQStarter {
 
@@ -163,6 +162,7 @@ public class CanalMQStarter {
                 canalDestination.setDynamicTopic(mqConfig.getDynamicTopic());
                 canalDestination.setPartitionsNum(mqConfig.getPartitionsNum());
                 canalDestination.setPartitionHash(mqConfig.getPartitionHash());
+                canalDestination.setDynamicTopicPartitionNum(mqConfig.getDynamicTopicPartitionNum());
 
                 canalServer.subscribe(clientIdentity);
                 logger.info("## the MQ producer: {} is running now ......", destination);
@@ -193,21 +193,54 @@ public class CanalMQStarter {
                                 public void rollback() {
                                     canalServer.rollback(clientIdentity, batchId);
                                 }
+
+                                @Override
+                                public void alarm(AlarmType alarmType, String message, Throwable throwable) {
+                                    canalInstance.getAlarmHandler().sendAlarm(destination, Alarm.buildJson(alarmType, message, throwable));
+                                }
                             }); // 发送message到topic
                         } else {
                             try {
                                 Thread.sleep(100);
-                            } catch (InterruptedException e) {
+                            } catch (InterruptedException ignore) {
                                 // ignore
                             }
                         }
 
+                    } catch (CanalMetaManagerException e) {
+                        logger.error(e.getMessage(), e);
+                        canalInstance.getAlarmHandler().sendAlarm(destination, Alarm.buildJson(AlarmType.META_FIRSTLY, e));
+                    } catch (com.alibaba.otter.canal.server.exception.CanalServerException e) {
+                        logger.error(e.getMessage(), e);
+                        AlarmType alarmType = AlarmType.UNKNOWN;
+                        if (StringUtils.isNotBlank(e.getMessage())) {
+                            if (e.getMessage().startsWith("destination:")) {
+                                alarmType = AlarmType.DESTINATION_SHOULD_START;
+                            } else if (e.getMessage().startsWith("clientId:")) {
+                                alarmType = AlarmType.CLIENT_ID_LASTEST_BATCH;
+                            } else if (e.getMessage().startsWith("ack error")) {
+                                alarmType = AlarmType.ACK_ERROR;
+                            } else if (e.getMessage().startsWith("rollback error")) {
+                                alarmType = AlarmType.ROLLBACK_ERROR;
+                            } else if (e.getMessage().startsWith("ClientIdentity:")) {
+                                alarmType = AlarmType.CLIENT_IDENTITY_SHOULD_SUBSCRIBE;
+                            } else if (e.getMessage().startsWith("can't find destination:")) {
+                                alarmType = AlarmType.DESTINATION_CONFIG_NOT_FOUND;
+                            }
+                        }
+                        canalServer.getCanalInstances().get(destination).getAlarmHandler().sendAlarm(destination, Alarm.buildJson(alarmType, e));
                     } catch (Exception e) {
                         logger.error(e.getMessage(), e);
+                        canalInstance.getAlarmHandler().sendAlarm(destination, Alarm.buildJson(AlarmType.UNKNOWN, e));
                     }
                 }
             } catch (Exception e) {
                 logger.error("process error!", e);
+                try {
+                    canalServer.getCanalInstances().get(destination)
+                            .getAlarmHandler().sendAlarm(destination, Alarm.buildJson(AlarmType.PROCESS, e));
+                } catch (Exception ignore) {
+                }
             }
         }
     }
